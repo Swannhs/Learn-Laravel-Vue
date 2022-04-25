9000f862fd1956eaeaac2a50ca1315f3dc45e7cf
Swann <princeapurbask@gmail.com>
2022-04-26 02:51:47 +0600

    Material sass file added

--- resources/js/index.vue
+++ resources/js/index.vue
@@ -1,6 +1,10 @@
 <template>
-<div>
-    hello
+<div class="container">
+    <div class="card">
+        <div class="card-body">
+            <h1>Hello from custom css</h1>
+        </div>
+    </div>
 </div>
 </template>
 
