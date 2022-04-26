2ebd87ccfc3f00d8ba43377b3fe53ba6ff76ee21
Swann <princeapurbask@gmail.com>
2022-04-26 10:00:01 +0600

    Component structure complete

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,16 @@
 import router from "./routes";
-import index from './index';
+import Index from './index';
+import VueRouter from "vue-router";
+import BootstrapVue from "bootstrap-vue";
 
 window.Vue = require('vue').default;
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
 
 const app = new Vue({
     el: '#app',
     router,
     components: {
-        index
+        index: Index
     }
 });
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">
+                        <i class="fas fa-home"></i>
+                        Home
+                    </h4>
+                </div>
+                <div class="card-body">
+                    <p>
+                        This is the home page.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
