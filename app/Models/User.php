38e70f41623287888491b18e6a004e4bb851cf0c
Swannhs <princeapurbask@gmail.com>
2022-05-01 01:28:50 +0600

    Trying to add role base auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,53 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles()
+    {
+        return $this
+            ->belongsToMany(Role::class)
+            ->withTimestamps();
+    }
+
+    public function users()
+    {
+        return $this
+            ->belongsToMany(User::class)
+            ->withTimestamps();
+    }
+
+    public function authorizeRoles($roles)
+    {
+        if (is_array($roles)) {
+            return $this->hasAnyRole($roles) ||
+                abort(401, 'This action is unauthorized.');
+        }
+
+        return $this->hasRole($roles) ||
+            abort(401, 'This action is unauthorized.');
+    }
+
+    public function hasAnyROle($roles)
+    {
+        if (is_array($roles)) {
+            foreach ($roles as $role) {
+                if ($this->hasRole($role)) {
+                    return true;
+                }
+            }
+        } else {
+            if ($this->hasRole($roles)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function hasRole($role)
+    {
+        if ($this->roles()->where('name', $role)->first()) {
+            return true;
+        }
+        return false;
+    }
 }
