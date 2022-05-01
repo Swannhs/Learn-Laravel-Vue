27d3b2f42eef95d3ea9d7d3105185bcf6ac60f37
Swannhs <princeapurbask@gmail.com>
2022-05-01 12:57:06 +0600

    Changed project idea to ecommerce project

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +18,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('posts', PostController::class)->only(['index', 'show']);
