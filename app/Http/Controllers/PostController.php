0605d5f053eb7d62ef01bf08419211e0a57f6d91
Swann <princeapurbask@gmail.com>
2022-06-19 12:10:49 +0600

    Post controller index with comment, vote, user relation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PostController extends Controller
 {
@@ -12,15 +13,40 @@ class PostController extends Controller
      *
      * @return Post[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return Post::all();
+        $response = array();
+        $response['status'] = 'success';
+        $response['total'] = Post::paginate($request->input('items'))->count();
+
+        $data = array();
+
+        foreach (Post::with('user', 'comment', 'vote')->paginate($request->input('items'), '*', 2) as $post) {
+            $item = array();
+
+            $author = array();
+            $author['id'] = $post->user->id;
+            $author['name'] = $post->user->name;
+
+            $item['id'] = $post->id;
+            $item['author'] = $author;
+            $item['title'] = $post->title;
+            $item['slug'] = $post->slug;
+            $item['comments'] = count($post->comment);
+            $item['vote'] = $post->vote;
+
+            array_push($data, $item);
+        }
+
+        $response['results'] = $data;
+
+        return $response;
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -31,7 +57,7 @@ class PostController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($slug)
@@ -42,8 +68,8 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -54,7 +80,7 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
