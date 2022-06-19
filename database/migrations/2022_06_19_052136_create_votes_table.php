9921066a4c0fae657c2a2a4ac3ad4f34df32f265
Swann <princeapurbask@gmail.com>
2022-06-19 12:09:14 +0600

    Created post, comment, vote migrations

--- database/migrations/2022_06_19_052136_create_votes_table.php
+++ database/migrations/2022_06_19_052136_create_votes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateVotesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('votes', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('slug');
-            $table->text('content');
             $table->foreignId('user_id')->constrained();
+            $table->foreignId('post_id')->constrained();
+            $table->boolean('up')->default(false);
+            $table->boolean('down')->default(false);
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('votes');
     }
 }
